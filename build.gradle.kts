b0e023ceebe3c3861b1e1d0adc93c3f6b76e6586
vha4493 <vha4493@gmail.com>
2026-09-13 12:54:57 -0700

    its- its im i need to be committing more often but its showing! that we have issues!

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.detekt) apply false
+    //you would run the thing and then in detekt-report.html click the link and it'd show you everything(overwhelmingly) wrong
 }
