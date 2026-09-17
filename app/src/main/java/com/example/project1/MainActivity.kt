c7fc7209e50928fc55d9d1431be222f754d065eb
gojoshin <joshin@csumb.edu>
2026-09-16 21:04:43 -0700

    imports for Foursquare API pt5

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -30,6 +30,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.project1.database.AppDatabase
+import com.example.project1.network.FoursquareRepository
+import com.example.project1.network.Restaurant
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
