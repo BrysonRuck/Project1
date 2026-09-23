123f1effa9cb9a546f735ab9200862b3752d35cf
vha4493 <vha4493@gmail.com>
2026-09-23 11:38:18 -0700

    unrolling back some AccCreation

--- app/src/main/java/com/example/project1/CreateAccActivity.kt
+++ app/src/main/java/com/example/project1/CreateAccActivity.kt
@@ -1,27 +1,77 @@
 package com.example.project1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import com.example.project1.database.AppDatabase
+import com.example.project1.database.UserDao
 import com.example.project1.ui.theme.Project1Theme
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.example.project1.MainActivity
+import com.example.project1.database.UserEntity
 
-class CreateAccActivity : ComponentActivity()  {
+
+class CreateAccActivity : ComponentActivity() {
+    //This method's responsibility is to setup fields and button clicks
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-    //note as of 9/9/26: Victoria made the redirect but there is no logic here yet
-    //TODO (later tonight prolly): make the create account front-end
-}
-@Composable
-fun CreateAccPage() {
-    Column() { }
-}
-@Preview(showBackground = true)
-@Composable
-fun CreateAccPagePreview() {
-    Project1Theme {
-        FavoritePage()
+        enableEdgeToEdge()
+
+        val userDao = AppDatabase
+            .getDatabase(this)
+            .userDao()
+
+        setContent {
+            Project1Theme {
+                val snackbarHostState = remember {
+                    SnackbarHostState()
+                }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState)
+                    }
+                ) { innerPadding ->
+                    CreateAccScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        onBack = {
+                            //navigation
+                        },
+                        onCreateAccount = { username, password, address ->
+                            createAcc(
+                                userDao = userDao,
+                                username = username,
+                                password = password,
+                                address = address,
+                                snackbarHostState = snackbarHostState
+                            )
+                        }
+                    )
+                }
+            }
+        }
     }
 }
